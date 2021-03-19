e2dae8da650c8bcb00451eac0cab495602e68793
Kira2125 <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2021-03-19 19:19:26 +0300

    EveryBody

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello my Friend!";
+        message = "Hello EveryBody!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
