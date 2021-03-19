ee881f9268bf8632ee05e99f743e0cd95a8fb887
Kira2125 <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2021-03-19 19:47:17 +0300

    github webhook

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello WebHook!";
+        message = "Github WebHook!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
